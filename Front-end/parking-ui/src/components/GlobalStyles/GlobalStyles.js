85511cf646a05df075ad6347c1c31f417c3cfc7b
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-06-18 00:34:31 +0700

    config sass

--- Front-end/parking-ui/src/components/GlobalStyles/GlobalStyles.js
+++ Front-end/parking-ui/src/components/GlobalStyles/GlobalStyles.js
@@ -1,12 +1,12 @@
-import PropTypes from 'prop-types'
-import './GlobalStyles.scss'
+import PropTypes from 'prop-types';
+import './GlobalStyles.scss';
 
-function GlobalStyles({children}) {
+function GlobalStyles ({ children }) {
     return children;
 }
 
 GlobalStyles.propTypes = {
     children: PropTypes.node.isRequired,
-}
+};
 
 export default GlobalStyles;
